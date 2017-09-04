4500fa78e59ac5c10707ca20595ffb25744c80dc
sph@bizsaas.cn <yUhU13552247687>
2017-09-04 19:07:53 +0800

    整合sso单点登录系统-2

---
+++ 18taotao-user/20taotao-user-service/src/main/java/study/project/user/service/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package study.project.user.service;
+
+/**
+ *
+ * Created by panhusun on 2017/9/4.
+ */
+public class UserServiceImpl {
+}
---
+++ 21taotao-user-sso-web/src/main/java/study/project/user/sso/controller/UserController.java
@@ -0,0 +1,9 @@
+package study.project.user.sso.controller;
+
+/**
+ *
+ * Created by panhusun on 2017/9/4.
+ */
+public class UserController {
+
+}
