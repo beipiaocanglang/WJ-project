4b8537f50a7b0daa731ac0f940f0e911bf8e2bf0
sph@bizsaas.cn <yUhU13552247687>
2017-09-14 16:15:50 +0800

    测试php项目

--- phpproject/conn.php
+++ phpproject/conn.php
@@ -1,15 +1,16 @@
 <?php
-$dbhost = 'rm-bp1s9co7ux0293175.mysql.rds.aliyuncs.com';  // mysql服务器主机地址
-$dbuser = 'r1yc477ai2';            // mysql用户名
-$dbpass = 'xhJSzc@1';          // mysql用户名密码
+    $mysql_server_name='127.0.0.1'; //改成自己的mysql数据库服务器
 
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+    $mysql_username='root'; //改成自己的mysql数据库用户名
 
-if(! $conn )
-{
-    die('连接失败: ' . mysqli_error($conn));
-}
-/*echo '连接成功<br />';*/
-// 设置编码，防止中文乱码
-mysqli_query($conn , "set names utf8");
+    $mysql_password='root'; //改成自己的mysql数据库密码
+
+    $mysql_database='php'; //改成自己的mysql数据库名
+
+    $conn=mysqli_connect($mysql_server_name, $mysql_username, $mysql_password) or die("error connecting"); //连接数据库
+
+    echo '连接成功<br/>';
+
+    mysqli_query($conn, "set names 'utf8'"); //数据库输出编码 应该与你的数据库编码保持一致.南昌网站建设公司百恒网络PHP工程师建议用UTF-8 国际标准编码.
+
 ?>
--- phpproject/reg.php
+++ phpproject/reg.php
@@ -1,48 +1,45 @@
 <?php
-header("Content-type: text/html; charset=utf-8");
-if(isset($_POST['submit'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $email = $_POST['email'];
-    $mobile = $_POST['mobile'];
+    header("Content-type: text/html; charset=utf-8");
+    if(isset($_POST['submit'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+        $mobile = $_POST['mobile'];
 
 
-//注册信息判断
-    if(!preg_match('/^[\w\x80-\xff]{3,15}$/', $username)){
-        exit('<script>alert("用户名不符合规定。");history.back(-1)</script>');
+    //注册信息判断
+        if(!preg_match('/^[\w\x80-\xff]{3,15}$/', $username)){
+            exit('<script>alert("用户名不符合规定。");history.back(-1)</script>');
+        }
+        if(strlen($password) < 6){
+            exit('<script>alert("密码长度不符合规定。");history.back(-1)</script>');
+        }
+        if(!preg_match("/^1[34578]\d{9}$/",$mobile)){
+            exit('<script>alert("手机号格式错误。");history.back(-1)</script>');
+        }
+        if(!preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/', $email)){
+            exit('<script>alert("电子邮箱格式错误。");history.back(-1)</script>');
+        }
+
+        include('conn.php');
+
+        $sql = "INSERT INTO PHP_user(username,password,email,mobile)VALUES('$username','$password','$email','$mobile')";
+
+        mysqli_select_db($conn, $mysql_database);//打开数据库
+
+        $retval = mysqli_query( $conn, $sql );//执行查询
+
+        if(!$retval ) {
+            //die('无法插入数据: ' . mysqli_error($conn));
+            die('<script>alert("用户注册失败");history.back(-1)</script>'. mysqli_error($conn));
+        }
+
+        //echo "数据插入成功\n";
+        echo '<script>alert("用户注册成功");location.href = "login.html"</script>';
+
+        mysqli_close($conn);//关闭数据库
+    }else {
+        exit('<script>location.href=\'login.html\'</script>');
     }
-    if(strlen($password) < 6){
-        exit('<script>alert("密码长度不符合规定。");history.back(-1)</script>');
-    }
-    if(!preg_match("/^1[34578]\d{9}$/",$mobile)){
-        exit('<script>alert("手机号格式错误。");history.back(-1)</script>');
-    }
-    if(!preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/', $email)){
-        exit('<script>alert("电子邮箱格式错误。");history.back(-1)</script>');
-    }
-
-    include('conn.php');
-
-    $sql = "INSERT INTO users(username,password,email,mobile)VALUES('$username','$password','$email','$mobile')";
-
-
-    mysqli_select_db($conn, 'r1yc477ai2' );
-
-    $retval = mysqli_query( $conn, $sql );
-
-
-
-    if(!$retval )
-    {
-        //die('无法插入数据: ' . mysqli_error($conn));
-        die('<script>alert("用户注册失败");history.back(-1)</script>'. mysqli_error($conn));
-    }
-//echo "数据插入成功\n";
-    echo '<script>alert("用户注册成功");location.href = "login.html"</script>';
-
-    mysqli_close($conn);
-}else {
-    exit('<script>location.href=\'login.html\'</script>');
-}
 
 ?>
