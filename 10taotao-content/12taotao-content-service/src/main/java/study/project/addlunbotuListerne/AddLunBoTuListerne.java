1d20c7436aa523744f975862a5a2b7218aa2eb8e
sph@bizsaas.cn <yUhU13552247687>
2017-09-22 19:49:20 +0800

    商城首页轮播图添加MQ   轮播图实时同步 -1

---
+++ 10taotao-content/12taotao-content-service/src/main/java/study/project/addlunbotuListerne/AddLunBoTuListerne.java
@@ -0,0 +1,15 @@
+package study.project.addlunbotuListerne;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+/**
+ * 添加轮播图的MQ
+ * Created by panhusun on 2017/9/22.
+ */
+public class AddLunBoTuListerne implements MessageListener{
+    @Override
+    public void onMessage(Message message) {
+
+    }
+}
